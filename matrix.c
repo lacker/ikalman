d3d99cb0323c70a7b497fc36c0ba79b21b86cfee
Kevin Lacker <lacker@gmail.com>
2009-07-19 09:04:49 -0700

    Copying and printing matrices.

--- matrix.c
+++ matrix.c
@@ -1,2 +1,27 @@
 /* Matrix math. */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+
+void m_copy(Number** source, Number** destination,
+	    int m, int n) {
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      destination[i][j] = source[i][j];
+    }
+  }
+}
+
+void m_print(Number** matrix, int m, int n) {
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (j > 0) {
+	printf(" ");
+      }
+      printf("6.2f", matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
--- matrix.h
+++ matrix.h
@@ -1,8 +1,14 @@
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
 
-/* Matrix math. */
+/* The type we use for all math. */
+typedef Number double;
 
+/* Copy a m*n matrix. */
+void m_copy(Number** source, Number** destination,
+	    int m, int n);
 
+/* Pretty-print a m*n matrix. */
+void m_print(Number** matrix, int m, int n);
 
 #endif
